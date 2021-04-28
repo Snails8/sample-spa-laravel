2a8c438875190995fb7613d0cee1a840284fc8c3
S-tanishi <tanichi.u.n.20@gmail.com>
2021-04-28 16:24:32 +0900

    コンテナ間通信のため共通originの設定

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+//    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://web'],
 
     'allowed_origins_patterns' => [],
 
